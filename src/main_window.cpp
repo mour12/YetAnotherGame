1d7d355d43e960af5656be6c46fa62b25f77da3b
Andrei Murashov <andrei.murashov@gmail.com>
2016-11-17 21:29:46 +0300

    Codestyle fixes

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -10,24 +10,24 @@ typedef void (QWidget::*QWidgetVoidSlot)();
 MainWindow::MainWindow()
 {
   auto centralWidget = new QWidget(this);
-  QHBoxLayout *mainLayout = new QHBoxLayout(centralWidget);
-  QGroupBox *box = new QGroupBox("Main menu", centralWidget);
-  QGridLayout *boxLayout = new QGridLayout(box);
+  QHBoxLayout * mainLayout = new QHBoxLayout(centralWidget);
+  QGroupBox * box = new QGroupBox("Main menu", centralWidget);
+  QGridLayout * boxLayout = new QGridLayout(box);
   mainLayout->addWidget(box);
 
   box->setFixedSize(300, 400);
   box->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
   box->setAlignment(Qt::AlignCenter);
 
-  QPushButton *startGame = new QPushButton("Start new game");
+  QPushButton * startGame = new QPushButton("Start new game");
   connect(startGame, SIGNAL(clicked()), SLOT(StartGame()));
   boxLayout->addWidget(startGame, 0, 0);
 
-  QPushButton *settings = new QPushButton("Settings");
+  QPushButton * settings = new QPushButton("Settings");
   connect(settings, SIGNAL(clicked()), SLOT(OpenSettings()));
   boxLayout->addWidget(settings, 1, 0);
 
-  QPushButton *exitGame = new QPushButton("Exit");
+  QPushButton * exitGame = new QPushButton("Exit");
   connect(exitGame, SIGNAL(clicked()), SLOT(close()));
   boxLayout->addWidget(exitGame, 2, 0);
 
@@ -41,7 +41,7 @@ void MainWindow::StartGame()
 
 void MainWindow::OpenSettings()
 {
-  SettingsWindow *settings = new SettingsWindow();
+  SettingsWindow * settings = new SettingsWindow();
   settings->show();
   settings->showFullScreen();
 }
